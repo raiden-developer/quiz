b7714c13f07849ab14c3eaba2a6350ea6fff5c81
raiden-developer <raiden226@list.ru>
2022-07-23 21:18:24 +0800

    feat: add 5 slides and basic settings

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,24 @@
-console.log('1')
+import Swiper, {Scrollbar, Navigation, EffectFade} from 'swiper'
+import 'simplebar'
+
+Swiper.use([EffectFade])
+
+const swiper = new Swiper('.swiper', {
+  threshold: 10,
+  spaceBetween: 100,
+  speed: 250,
+  effect: 'fade',
+  initialSlide: 4,
+  fadeEffect: {
+    crossFade: true
+  },
+
+  modules: [Scrollbar, Navigation],
+  scrollbar: {
+    el: '.swiper-scrollbar'
+  },
+  navigation: {
+    nextEl: '.m-quiz__btn--next',
+    prevEl: '.m-quiz__btn--prev'
+  }
+})
