584a56ef3ff93ebdbed0edba41a866ccc29e0313
raiden-developer <raiden226@list.ru>
2022-07-24 01:15:49 +0800

    feat: add new slides and almost complete markup

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,7 @@ const swiper = new Swiper('.swiper', {
   spaceBetween: 100,
   speed: 250,
   effect: 'fade',
-  initialSlide: 4,
+  initialSlide: 0,
   fadeEffect: {
     crossFade: true
   },
@@ -22,3 +22,49 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.m-quiz__btn--prev'
   }
 })
+
+const connectionTypes = [
+  {
+    name: 'whatsapp',
+    text: 'Ваш номер в Whatsapp',
+    nameAttr: 'Whatsapp номер'
+  },
+  {
+    name: 'telegram',
+    text: 'Ваш номер в Telegram',
+    nameAttr: 'Telegram номер'
+  },
+  {
+    name: 'phone',
+    text: 'Ваш номер телефона',
+    nameAttr: 'Номер телефона'
+  }
+]
+
+function selectConnectionType(connectionTypes, selector, targetSelector) {
+  const elements = document.querySelectorAll(selector)
+  const target = document.querySelector(targetSelector)
+  let lastElement = null
+
+  elements.forEach((el) => {
+    if (el.classList.contains('active')) lastElement = el
+
+    const currentConnection = connectionTypes.find((connection) => {
+      return connection.name === el.getAttribute('data-connection')
+    })
+
+    el.addEventListener('click', () => {
+      if (lastElement) lastElement.classList.remove('active')
+      target.name = currentConnection.nameAttr
+      target.placeholder = currentConnection.text
+      el.classList.add('active')
+      lastElement = el
+    })
+  })
+}
+
+selectConnectionType(
+  connectionTypes,
+  '.m-quiz__social-btn',
+  '.m-quiz__final-number input'
+)
